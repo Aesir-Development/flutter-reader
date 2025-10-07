602e60de87fdf10a181a996ac96b99784302dd25
HollowHuu <nico@mmx.dk>
2025-10-07 13:25:25 +0200

    Fully working FlameComics plugin now.
    This should later be replaced with a placeholder plugin, but this should
    work for now.

--- lib/services/plugin_service.dart
+++ lib/services/plugin_service.dart
@@ -16,8 +16,12 @@ class PluginService {
         .convert(await rootBundle.loadString('plugins/PluginManifest.json'));
 
     List<String> libs = [];
-    for (var path in pluginPaths["libs"]) {
-      libs.add(await rootBundle.loadString('$path'));
+    try {
+      for (var path in pluginPaths["libs"]) {
+        libs.add(await rootBundle.loadString('$path'));
+      }
+    } catch (err) {
+      debugPrint("$err");
     }
 
     var res = lvm.eval(libs.join("\n"));
@@ -39,56 +43,13 @@ class PluginService {
   }
 
   static runTest() async {
-    // String result = lvm.exec("http_request", "https://flamecomics.xyz");
-
-    // Test to check blocking
-
-    lvm.eval('local result = ""');
-
-    // int count = 10;
-    // for (int i = 0; i < count; i++) {
-    //   lvm.eval("""
-    //     local co$i = coroutine.create(function()
-    //       http_request("https://flamecomics.xyz")
-    //       print($i);
-    //     end)
-
-    //     if coroutine.status(co${i - 1}) ~= "dead" or $i == 0 then
-    //       coroutine.resume(co$i)
-    //     end
-    //   """);
-    // }
-    // lvm.eval("""
-    //   function Test()
-    //     for i = 0, 9 do
-    //       _G["co"..i] = coroutine.create(function()
-    //         local res = http_request("https://flamecomics.xyz")
-    //         print(res)
-    //       end)
-    //     end
-
-    //     for i = 0, 9 do
-    //       if coroutine.status(_G["co"..i]) ~= "dead" then
-    //         coroutine.resume(_G["co"..i])
-    //       end
-    //     end
-    //   end
-
-    //   function Test2(s1, s2)
-    //     print(s1..s2)
-    //     return "Success"
-    //   end
-
-    //   """);
-
     List<Pointer<Variant>> args = [
-      lvm.stringArg("Hello"),
-      lvm.stringArg("World!")
+      lvm.stringArg("solo"),
     ];
 
     var result = await lvm.exec("FLAMECOMICS.test", []);
     // lvm.eval("print(FLAMECOMICS:test())");
 
-    debugPrint("Response length: ${result.length}");
+    debugPrint(result);
   }
 }
